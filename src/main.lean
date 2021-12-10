d1f0d1ff1bb0c654527b389a47d1afb834c87a27
eduardoFreire <dudufreirecpp@gmail.com>
2021-12-10 19:12:32 -0300

    started using structure

--- src/main.lean
+++ src/main.lean
@@ -5,78 +5,104 @@ import data.set
 
 noncomputable theory
 
-variable X : Type
-variable d : X → X → ℝ
+def dist_refl {X : Type} (d : X → X → ℝ)   := ∀(x y : X), d x y = 0 ↔ x = y
+def dist_symm {X : Type} (d : X → X → ℝ)   := ∀(x y : X), d x y = d y x
+def dist_triangle{X : Type}(d : X → X → ℝ) := ∀(x y z : X), d x y ≤  d x z + d z y
 
+structure metric_space (X : Type) := 
+(d : X → X → ℝ)
+(refl     : dist_refl d)
+(symm     : dist_symm d)
+(triangle : dist_triangle d)
 
-def dist_refl := ∀(x y: X), d x y = 0 ↔ x = y
-def dist_symm := ∀(x y : X), d x y = d y x
-def dist_triangle := ∀(x y z : X), d x y ≤  d x z + d z y
-
-def is_metric_space := 
-dist_refl X d ∧ dist_symm X d ∧ dist_triangle X d 
+variables {X : Type} (mX : metric_space X)
 
 
 def dist_real : ℝ → ℝ → ℝ := λ (a b : ℝ), |a - b|
 
-lemma dist_real_to_zero : ∀(x : ℝ), dist_real x 0 = |x| :=
+@[simp]lemma dist_real_to_zero : ∀(x : ℝ), dist_real x 0 = |x| :=
 begin 
   intro x,
   unfold dist_real,
   rw sub_zero x,
 end
 
-lemma real_is_metric_space : is_metric_space ℝ dist_real := 
-begin
-  split,
-  {
+def mR : metric_space ℝ := {
+  d := dist_real,
+  refl := 
+  begin 
   intros x y,
   unfold dist_real,
 
   simp only [abs_eq_zero],
   exact sub_eq_zero,
-  },
+  end,
 
-  split,
-  {
-  intros x y,
-  exact abs_sub_comm x y,
-  },
+  symm := 
+  begin 
+    intros x y,
+    exact abs_sub_comm x y,
+  end,
 
-  intros x y z,
-  exact abs_sub_le x z y,
-end
+  triangle :=
+  begin 
+    intros x y z,
+    exact abs_sub_le x z y,
+  end
+}
 
-constant hX : is_metric_space X d
-
-lemma dist_nonneg : 
-∀(x y : X), d x y  ≥ 0 := 
+@[simp] lemma mR_dist_is_dist_real : ∀(x y : ℝ), mR.d x y = |x - y| :=
 begin 
   intros x y,
-  have h1 := (hX X d).right.right x x y,
-  have h2 := ((hX X d).left x x).mpr (by refl),
+  refl,
+end
+
+@[simp] lemma dist_real_is_abs : ∀(x y : ℝ), dist_real x y = |x - y| :=
+begin 
+  intros x y,
+  refl,
+end
+
+@[simp]lemma dist_real_metric_to_zero : ∀(x : ℝ), mR.d x 0 = |x| :=
+begin 
+  intro x,
+  change mR.d with dist_real,
+  unfold dist_real,
+  rw sub_zero x,
+end
+
+
+lemma dist_nonneg : 
+∀(x y : X), mX.d x y  ≥ 0 := 
+begin 
+  intros x y,
+  have h1 := mX.triangle x x y,
+  have h2 := (mX.refl x x).mpr (by refl),
   rw h2 at h1,
-  rw ← (hX X d).right.left x y at h1,
+  rw ← mX.symm x y at h1,
   linarith,
 end
 
-lemma abs_of_dist (x y : X) : |d x y| = d x y :=
-abs_eq_self.mpr (dist_nonneg X d x y)
+lemma abs_of_dist (x y : X) : |mX.d x y| = mX.d x y :=
+begin 
+  simp,
+  exact dist_nonneg mX x y,
+end
 
 def is_cauchy (a : ℕ → X)
 := ∀(ε : ℝ)(hε : ε > 0),∃(N : ℕ),∀(n m : ℕ)(hn: n ≥ N)
-(hm: m ≥ N), d (a n) (a m)  < ε
+(hm: m ≥ N), mX.d (a n) (a m)  < ε
 
 
 def seq_lim (a : ℕ → X) (L : X) :=
 ∀(ε : ℝ)(hε : ε > 0),∃(N : ℕ),∀(n : ℕ)(hn: n ≥ N),
-d (a n) L < ε
+mX.d (a n) L < ε
 
 def seq_lim2 (a : ℕ → X) (L : X) :=
-seq_lim ℝ dist_real (λ(n : ℕ), d (a n) L) 0
+seq_lim mR (λ(n : ℕ), mX.d (a n) L) (0:ℝ)
 
 lemma seq_lim_defs (a : ℕ → X) (L : X) :
-seq_lim X d a L ↔ seq_lim2 X d a L :=
+seq_lim mX a L ↔ seq_lim2 mX a L :=
 begin 
   split,
   {
@@ -84,8 +110,8 @@ begin
     cases h ε hε with N hN,
     use N,
     intros n hn,
-    rw dist_real_to_zero,
-    rw abs_of_dist X d,
+    rw dist_real_metric_to_zero,
+    rw abs_of_dist,
     exact hN n hn,
   },
   {
@@ -94,60 +120,56 @@ begin
     use N,
     intros n hn,
     specialize hN n hn,
-    rw dist_real_to_zero at hN,
-    rw abs_of_dist X d at hN,
+    rw dist_real_metric_to_zero at hN,
+    rw abs_of_dist at hN,
     exact hN,
   },
 end
 
+
 lemma lim_of_mult_const_seq (x : ℕ → ℝ) (L : ℝ) 
-(hx : seq_lim ℝ dist_real x L) (k : ℝ) : seq_lim ℝ dist_real (λn, k * (x n)) (k*L) :=
+(hx : seq_lim mR x L) (k : ℝ) : seq_lim mR (λn, k * (x n)) (k*L) :=
 begin 
   intros ε hε,
-    by_cases |k| = 0,
-    {
-    use 0,
-    intros n hn,
-    have : k = 0 := abs_eq_zero.mp h,
-    rw this,
-    change (λ (n : ℕ), 0 * x n) n with 0 * x n,
-    rw zero_mul (x n),
-    rw zero_mul L,
-    have : dist_real 0 0 = |(0:ℝ)-(0:ℝ)| := by refl,
-    rw this,
-    have : (0:ℝ)-(0:ℝ)=0 := sub_zero 0,
-    rw this,
-    rw abs_zero,
-    exact hε,
-  },
+  by_cases |k| = 0,
   {
-   have hk_pos : |k| > 0 := (ne.symm h).le_iff_lt.mp (abs_nonneg k), 
-   cases hx (ε/|k|) (div_pos hε hk_pos) with N hN,
-   use N,
-   intros n hn,
-   change |k * (x n) - (k * L)| < ε,
-   ring_nf,
-   rw abs_mul (x n - L) k,
-   rw (lt_div_iff hk_pos).symm,
-   exact hN n hn,
-  },
-  
-
+  use 0,
+  intros n hn,
+  have : k = 0 := abs_eq_zero.mp h,
+  rw this,
+  change (λ (n : ℕ), 0 * x n) n with 0 * x n,
+  rw zero_mul (x n),
+  rw zero_mul L,
+  have : dist_real 0 0 = |(0:ℝ)-(0:ℝ)| := by refl,
+  simp at *,
+  exact hε,
+},
+{
+  have hk_pos : |k| > 0 := (ne.symm h).le_iff_lt.mp (abs_nonneg k), 
+  cases hx (ε/|k|) (div_pos hε hk_pos) with N hN,
+  use N,
+  intros n hn,
+  change |k * (x n) - (k * L)| < ε,
+  ring_nf,
+  rw abs_mul (x n - L) k,
+  rw (lt_div_iff hk_pos).symm,
+  exact hN n hn,
+},
 end
 
 def seq_converges (a : ℕ → X) :=
-∃(L : X), seq_lim X d a L
+∃(L : X), @seq_lim X mX a L
 
-def is_complete : Prop := ∀(x : ℕ → X)(hx : is_cauchy X d x), seq_converges X d x
+def is_complete : Prop := ∀(x : ℕ → X)(hx : is_cauchy mX x), seq_converges mX x
 
 def seq_equiv (a b: ℕ → X) :=
-∀(ε : ℝ)(hε : ε > 0),∃(N : ℕ),∀(n : ℕ)(hn: n ≥ N), d (a n) (b n) < ε
+∀(ε : ℝ)(hε : ε > 0),∃(N : ℕ),∀(n : ℕ)(hn: n ≥ N), mX.d (a n) (b n) < ε
 
 def seq_equiv2
  (a b: ℕ → X) :=
-seq_lim ℝ dist_real (λ(n: ℕ), d (a n) (b n)) 0
+seq_lim mR (λ(n: ℕ), mX.d (a n) (b n)) 0
 
-lemma seq_equiv_defs (a b : ℕ → X): seq_equiv X d a b ↔ seq_equiv2 X d a b :=
+lemma seq_equiv_defs (a b : ℕ → X): seq_equiv mX a b ↔ seq_equiv2 mX a b :=
 begin 
   split,
   {
@@ -155,9 +177,7 @@ begin
     cases h ε hε with N hN,
     use N,
     intros n hn,
-    rw dist_real_to_zero,
-    have obv : (λ (n : ℕ), d (a n) (b n)) n = d (a n) (b n) := by refl,
-    rw obv,
+    simp,
     rw abs_of_dist,
     exact hN n hn,
   },
@@ -167,33 +187,31 @@ begin
     use N,
     intros n hn,
     specialize hN n hn,
-    rw dist_real_to_zero at hN,
-    have obv : (λ (n : ℕ), d (a n) (b n)) n = d (a n) (b n) := by refl,
-    rw obv at hN,
+    simp at *,
     rw abs_of_dist at hN,
-    exact hN
+    exact hN,
   },
 end
 
 lemma seq_equiv_almost_symm (a b: ℕ → X) : 
-seq_equiv X d a b → seq_equiv X d b a :=
+seq_equiv mX a b → seq_equiv mX b a :=
 begin 
   intros h ε hε,
   cases h ε hε with N hN,
   use N,
   intros n hn,
-  have d_symm := (hX X d).right.left (b n) (a n),
+  have d_symm := mX.symm (b n) (a n),
   rw d_symm,
   exact hN n hn,
 end
 
 lemma seq_equiv_symm (a b: ℕ → X) : 
-seq_equiv X d a b ↔ seq_equiv X d b a := 
-⟨seq_equiv_almost_symm X d a b, seq_equiv_almost_symm X d b a⟩
+seq_equiv mX a b ↔ seq_equiv mX b a := 
+⟨seq_equiv_almost_symm mX a b, seq_equiv_almost_symm mX b a⟩
 
 
 lemma equiv_is_cauchy (x : ℕ → X) (y : ℕ → X)
-(hx : is_cauchy X d x) (hxy: seq_equiv X d x y) : is_cauchy X d y :=
+(hx : is_cauchy mX x) (hxy: seq_equiv mX x y) : is_cauchy mX y :=
 begin 
   intros ε hε,
   unfold is_cauchy at hx,
@@ -204,9 +222,9 @@ begin
 
   intros n m hn hm,
   
-  have hcalc1 := (hX X d).right.right (x n) (y m) (x m),
+  have hcalc1 := mX.triangle (x n) (y m) (x m),
 
-  have hcalc2 : d (y n) (x n) + d (x n) (x m) + d (x m) (y m) < ε,
+  have hcalc2 : mX.d (y n) (x n) + mX.d (x n) (x m) + mX.d (x m) (y m) < ε,
   {
     have hn1 : n ≥ N₁ := le_of_max_le_left hn,
     have hn2 : n ≥ N₂ := le_of_max_le_right hn,
@@ -214,7 +232,7 @@ begin
     have hm2 : m ≥ N₂ := le_of_max_le_right hm,
 
 
-    have symm1 := (hX X d).right.left (x n) (y n),
+    have symm1 := mX.symm (x n) (y n),
     rw ← symm1,
 
     have dxy1 := hN₂ n hn2,
@@ -223,14 +241,14 @@ begin
     linarith,
   },
 
-  exact calc d (y n) (y m) ≤ d(y n) (x n) + d(x n) (y m) 
-  : (hX X d).right.right (y n) (y m) (x n)
-  ... ≤ d (y n) (x n) + d (x n) (x m) + d (x m) (y m) : by linarith
+  exact calc mX.d (y n) (y m) ≤ mX.d (y n) (x n) + mX.d (x n) (y m) 
+  : mX.triangle (y n) (y m) (x n)
+  ... ≤ mX.d (y n) (x n) + mX.d (x n) (x m) + mX.d (x m) (y m) : by linarith
   ... < ε : hcalc2,
 end
 
 lemma seqs_equiv_if_same_limit (x y : ℕ → X) (L : X) 
-(hx : seq_lim X d x L) (hy : seq_lim X d y L) : seq_equiv X d x y :=
+(hx : seq_lim mX x L) (hy : seq_lim mX y L) : seq_equiv mX x y :=
 
 begin 
   intros ε hε,
@@ -241,23 +259,23 @@ begin
 
   have t1 := hN₁ n (le_of_max_le_left hn),
   have t2 := hN₂ n (le_of_max_le_right hn),
-  have t3 : d (x n) L + d (y n) L < ε := by linarith,
-  have t4 := (hX X d).right.right (x n) (y n) L,
+  have t3 : mX.d (x n) L + mX.d (y n) L < ε := by linarith,
+  have t4 := mX.triangle (x n) (y n) L,
 
-  rw (hX X d).right.left (y n) L at t3,
+  rw mX.symm (y n) L at t3,
 
-  calc d (x n) (y n) ≤ d (x n) L + d L (y n) : t4
+  calc mX.d (x n) (y n) ≤ mX.d (x n) L + mX.d L (y n) : t4
   ... < ε : t3,
 end
 
 theorem seq_squeeze (x y z : ℕ → ℝ) (L : ℝ)
- (hx : seq_lim ℝ dist_real x L) (hz : seq_lim ℝ dist_real z L) 
+ (hx : seq_lim mR x L) (hz : seq_lim mR z L) 
  (hxy : ∀(n : ℕ), x n ≤ y n) (hyz : ∀(n : ℕ), y n ≤ z n) :
- seq_lim ℝ dist_real y L :=
+ seq_lim mR y L :=
 begin
   
   intros ε hε,
-  have hxz_equiv := seqs_equiv_if_same_limit ℝ dist_real x z L hx hz,
+  have hxz_equiv := seqs_equiv_if_same_limit mR x z L hx hz,
   cases hxz_equiv (ε/2) (by linarith) with N₁ hN₁,
   cases hx (ε/2) (by linarith) with N₂ hN₂,
   use max N₁ N₂,
@@ -288,13 +306,13 @@ begin
   have t9 : | (y n) - (x n)| + |(x n) - L| < ε := by linarith,
   
   calc dist_real (y n) L ≤ dist_real (y n) (x n) + dist_real (x n) L : 
-  real_is_metric_space.right.right (y n) L (x n)
+  mR.triangle (y n) L (x n)
   ... < ε : t9,
 end
 
 lemma sum_of_lims_is_lim_of_sum (x y : ℕ → ℝ) (L1 L2 : ℝ)
- (hx : seq_lim ℝ dist_real x L1) (hy : seq_lim ℝ dist_real y L2) :
-  seq_lim ℝ dist_real (λ(n : ℕ), (x n) + (y n)) (L1 + L2) :=
+ (hx : seq_lim mR x L1) (hy : seq_lim mR y L2) :
+  seq_lim mR (λ(n : ℕ), (x n) + (y n)) (L1 + L2) :=
 begin 
   intros ε hε,
   cases hx (ε/2) (by linarith) with N₁ hN₁,
@@ -310,6 +328,7 @@ begin
 
   have temp : |(x n - L1)| + |(y n - L2)| =
    dist_real (x n) L1 + dist_real (y n) L2 := by refl,
+  simp at *,
 
   calc dist_real (x n + y n) (L1 + L2)  = |(x n + y n) - (L1 + L2)| : by refl
   ... = |(x n - L1) + (y n - L2)| : by ring_nf
@@ -318,51 +337,47 @@ begin
 end
 
 
-lemma equiv_seqs_same_limit (x y : ℕ → X) (hxy : seq_equiv X d x y) (L : X) :
- seq_lim X d x L → seq_lim X d y L
+lemma equiv_seqs_same_limit (x y : ℕ → X) (hxy : seq_equiv mX x y) (L : X) :
+ seq_lim mX x L → seq_lim mX y L
 :=
  begin 
    intro hx,
-   have dyL_nonneg : ∀(n : ℕ), 0 ≤ d (y n) L,
+   have dyL_nonneg : ∀(n : ℕ), 0 ≤ mX.d (y n) L,
    {
      intro n,
      apply dist_nonneg,
    },
 
-   have dyL_triangle := 
-   λ(n : ℕ), (hX X d).right.right (y n) L (x n),
+   have dyL_triangle := λ(n : ℕ), mX.triangle (y n) L (x n),
 
    let seq1 : ℕ → ℝ := λ(n : ℕ), 0,
-   let seq2 : ℕ → ℝ := λ(n : ℕ), d (y n) L,
-   let seq3 : ℕ → ℝ  := λ(n : ℕ), d (y n) (x n) + d (x n) L,
+   let seq2 : ℕ → ℝ := λ(n : ℕ), mX.d (y n) L,
+   let seq3 : ℕ → ℝ  := λ(n : ℕ), mX.d (y n) (x n) + mX.d (x n) L,
 
   have h12 : ∀(n : ℕ), seq1 n ≤ seq2 n := dyL_nonneg,
   have h23 : ∀(n : ℕ), seq2 n ≤ seq3 n := dyL_triangle,
 
-  have hseq1_to_0 : seq_lim ℝ dist_real seq1 0,
+  have hseq1_to_0 : seq_lim mR seq1 0,
   {
     intros ε hε,
     use 1,
     intros n hn,
-    unfold dist_real,
-    ring_nf,
-    have t : |(0:ℝ)| = 0 := abs_zero,
-    rw t,
+    simp,
     exact hε,
   },
 
-  have hseq3_to_0 : seq_lim ℝ dist_real seq3 0,
+  have hseq3_to_0 : seq_lim mR seq3 0,
   {
-    let temp1 := λ(n : ℕ), d (y n) (x n),
-    let temp2 := λ(n : ℕ), d (x n) L,
+    let temp1 := λ(n : ℕ), mX.d (y n) (x n),
+    let temp2 := λ(n : ℕ), mX.d (x n) L,
 
-    have temp1_to_0 : seq_lim ℝ dist_real temp1 0,
+    have temp1_to_0 : seq_lim mR temp1 0,
     {
       rw seq_equiv_symm at hxy,
       rw seq_equiv_defs at hxy,
       exact hxy,
     },
-    have temp2_to_0 : seq_lim ℝ dist_real temp2 0,
+    have temp2_to_0 : seq_lim mR temp2 0,
     {
       rw seq_lim_defs at hx,
       exact hx,
@@ -378,7 +393,7 @@ lemma equiv_seqs_same_limit (x y : ℕ → X) (hxy : seq_equiv X d x y) (L : X) 
   have squeeze := seq_squeeze seq1 seq2 seq3 0 hseq1_to_0 hseq3_to_0 h12 h23,
 
 
-  have t1 : seq_lim2 ℝ dist_real seq2 0,
+  have t1 : seq_lim2 mR seq2 0,
   {
     intros ε hε,
     specialize squeeze ε hε,
@@ -387,10 +402,7 @@ lemma equiv_seqs_same_limit (x y : ℕ → X) (hxy : seq_equiv X d x y) (L : X) 
     intros n hn,
     have t2 : (λ (n : ℕ), dist_real (seq2 n) (seq1 n)) n =
     dist_real (seq2 n) 0 := by refl,
-    rw t2,
-    rw dist_real_to_zero (seq2 n),
-    rw dist_real_to_zero (|seq2 n|),
-    rw abs_abs (seq2 n),
+    simp at *,
     specialize hN n hn,
     rw dist_real_to_zero (seq2 n) at *,
     exact hN,
@@ -430,7 +442,7 @@ def is_subseq (y x : ℕ → ℝ) : Prop := ∃(φ: ℕ → ℕ) (hφ : is_extra
 
 theorem monotone_convergence_increasing {x : ℕ → ℝ}
  (hx_increases : is_increasing_seq x)
-(hx_bounded_above : is_bounded_above_seq x) : seq_converges ℝ dist_real x :=
+(hx_bounded_above : is_bounded_above_seq x) : seq_converges mR x :=
 begin 
   let xSet := {z : ℝ | ∃n:ℕ, x n = z},
   have xSet_nonempty : xSet.nonempty,
@@ -487,14 +499,14 @@ begin
 
   have t1 : x n - s < ε  := by linarith,
   have t2 : -ε < x n - s := by linarith,
-  unfold dist_real,
+  simp,
   have almost : -ε < x n - s ∧ x n - s < ε := ⟨t2, t1⟩,
   exact abs_lt.mpr almost,
 end
 
 theorem monotone_convergence_decreasing {x : ℕ → ℝ}
  (hx_decreases : is_decreasing_seq x)
-(hx_bounded_below : is_bounded_below_seq x) : seq_converges ℝ dist_real x :=
+(hx_bounded_below : is_bounded_below_seq x) : seq_converges mR x :=
 begin 
   have hnew_seq_increasing : is_increasing_seq (λn, (-1) * (x n)),
   {
@@ -525,7 +537,7 @@ begin
   finish,
 end
 
-lemma cauchy_seq_of_reals_is_bounded (x : ℕ → ℝ) (hx : is_cauchy ℝ dist_real x) :
+lemma cauchy_seq_of_reals_is_bounded (x : ℕ → ℝ) (hx : is_cauchy mR x) :
 is_bounded_seq x := sorry
 
 def is_peak_term (n : ℕ) (x : ℕ → ℝ) : Prop := ∀(m : ℕ)(hm: m ≥ n), x m ≤ x n
@@ -597,7 +609,7 @@ end
 
 
 theorem bolzano_weierstrass (x: ℕ → ℝ) (hx : is_bounded_seq x) : 
- ∃(y : ℕ → ℝ) (hy : is_subseq y x), seq_converges ℝ dist_real y := 
+ ∃(y : ℕ → ℝ) (hy : is_subseq y x), seq_converges mR y := 
  begin 
    cases seq_has_monotone_subseq x with y hy,
    cases hy with hy_subseq hy_monotone,
@@ -611,7 +623,7 @@ theorem bolzano_weierstrass (x: ℕ → ℝ) (hx : is_bounded_seq x) :
    {exact monotone_convergence_decreasing hy_monotone hy_bounded.2},
  end
 
-theorem reals_are_complete : is_complete ℝ dist_real := 
+theorem reals_are_complete : is_complete mR := 
 begin 
   intros x hx,
   have subseq_converges := 
@@ -649,14 +661,14 @@ begin
 
 
   calc dist_real (x n) y_lim ≤ dist_real (x n) (y N) + dist_real (y N) y_lim :
-  real_is_metric_space.right.right (x n) y_lim (y N)
+  mR.triangle (x n) y_lim (y N)
   ... < ε : by linarith,
 end
 
 
 
-lemma cauchy_lim_of_dist (x y : ℕ → X) (hx : is_cauchy X d x) 
- (hx : is_cauchy X d y) : seq_converges ℝ dist_real (λn:ℕ, d (x n) (y n)) :=
+lemma cauchy_lim_of_dist (x y : ℕ → X) (hx : is_cauchy mX x) 
+ (hx : is_cauchy mX y) : seq_converges mR (λn:ℕ, mX.d (x n) (y n)) :=
  begin 
    sorry,
  end
